32edefb25688388040cfbe4d3863eb9cf729c468
mzaini30 <muhzaini30@gmail.com>
2021-12-11 13:47:20 +0800

    Darurat

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# mantap-hp
+# Mantap
+
+<p align='center'>
+	<img src='mantap.jpeg'>
+</p>
+
+Tools untuk optimasi CSS, JS, dan gambar-gambar supaya websitenya wuzz wuzz...
+
+## Persiapan
+
+Install Node JS
+
+## Instalasi
+
+Jalankan: `npm i -g mantap`
+
+## Menjalankan
+
+Letakkan file-file CSS, JS, dan gambar di folder `src`
+
+Jalankan di Terminal/Command Prompt: `mantap`
---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const recursive = require('recursive-readdir-sync')
+const {minify} = require('uglify-js')
+const {exec} = require('child_process')
+const cleanCss = require('clean-css')
+// const sharp = require('sharp')
+const {platform} = require('process')
+
+function createFolderIfNone(dirName) {
+	if (!fs.existsSync(dirName)){
+		fs.mkdirSync(dirName);
+	}
+}
+
+createFolderIfNone('./build')
+
+const files = recursive('src')
+
+const fileJs = [...files].filter(x => x.match(/\.js$/))
+const fileCss = [...files].filter(x => x.match(/\.css$/))
+// const fileGambar = [...files].filter(x => x.match(/\.(png|jpg|jpeg)$/i))
+
+for (let x of fileJs){
+	let filenya = fs.readFileSync(x, 'utf8')
+	filenya = minify(filenya)
+	fs.writeFileSync(x.replace('src', 'build'), filenya.code, 'utf8')
+}
+
+for (let x of fileCss){
+	let filenya = fs.readFileSync(x, 'utf8')
+	filenya = new cleanCss().minify(filenya)
+	fs.writeFileSync(x.replace('src', 'build'), filenya.styles, 'utf8')
+}
+
+/*
+for (let x of fileGambar){
+	if (platform == 'linux') {
+
+		let file = x.split('/').reverse()[0]
+		sharp(x).toFile(`build/${file.replace(/\..+$/, '.webp')}`)
+
+	} else if (platform == 'win32'){
+
+		let file = x.split('\\').reverse()[0]
+		sharp(x).toFile(`build\\${file.replace(/\..+$/, '.webp')}`)
+
+	}
+}
+*/
